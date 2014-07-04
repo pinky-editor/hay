8a4af4914810b34f8edb7b8c6637ba450b097c08
Meikel Brandmeyer <mb@kotka.de>
2014-07-04 21:33:48 +0200

    Start compiler implementation

--- src/main/clojure/hay/stack.clj
+++ src/main/clojure/hay/stack.clj
@@ -1,4 +1,19 @@
 (ns hay.stack)
 
-(def state
-  (atom {::namespaces {}}))
+(def world
+  (atom
+    {::namespaces {}
+     ::namespace  :haystack.core}))
+
+(defprotocol ^:private Item
+  (^:private emit      [this])
+  (^:private signature [this]))
+
+(extend-protocol Item
+  Object
+  (emit      [this] #(conj % this))
+  (signature [this] [0 1])
+
+  nil
+  (emit      [this] #(conj % this))
+  (signature [this] [0 1]))
