fa0266f563f969b16290203e7b822143034eca39
Meikel Brandmeyer <mb@kotka.de>
2014-07-05 16:28:53 +0200

    Add compile-signature utility

--- src/main/clojure/hay/stack.clj
+++ src/main/clojure/hay/stack.clj
@@ -14,6 +14,14 @@
       w
       (throw (ex-info "Unknown word" {:unkown-word word})))))
 
+(defn ^:private compile-signature
+  [sig]
+  (reduce (fn [[to-push _] x]
+            (if (= x '--)
+              (reduced [to-push (- (count sig) to-push 1)])
+              [(inc to-push) _]))
+          [0 0] sig))
+
 (defprotocol ^:private Item
   (^:private emit      [this])
   (^:private signature [this]))
